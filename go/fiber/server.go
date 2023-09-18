83e5b04659778834a2ff095847477025d98b0617
Sina T <sina.tamjidy@gmail.com>
2023-09-18 12:47:51 +0330

    add signin endpoint with golang-jwt

--- go/fiber/server.go
+++ go/fiber/server.go
@@ -1,13 +1,98 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"bytes"         // for having bytes object
+	"encoding/json" // for using json
+	"fmt"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v5"
+)
 
 func main() {
-    app := fiber.New()
+	app := fiber.New()
 
-    app.Get("/", func(c *fiber.Ctx) error {
-        return c.SendString("Hello, World!")
-    })
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	app.Get("/signin", Signin)
 
-    app.Listen(":3000")
+	app.Listen(":3000")
+}
+
+// Create the JWT key used to create the signature
+var jwtKey = []byte("my_secret_key")
+
+// For simplification, we're storing the users information as an in-memory map in our code
+var users = map[string]string{
+	"user1": "password1",
+	"user2": "password2",
+}
+
+// Create a struct to read the username and password from the request body
+type Credentials struct {
+	Password string `json:"password"`
+	Username string `json:"username"`
+}
+
+// Create a struct that will be encoded to a JWT.
+// We add jwt.RegisteredClaims as an embedded type, to provide fields like expiry time
+type Claims struct {
+	Username string `json:"username"`
+	jwt.RegisteredClaims
+}
+
+// Create the Signin handler
+func Signin(c *fiber.Ctx) error {
+	var creds Credentials
+
+	reader := bytes.NewReader(c.Body())
+	err := json.NewDecoder(reader).Decode(&creds)
+	if err != nil {
+		// If the structure of the body is wrong, return an HTTP error
+		msg := "bad data\n" + err.Error() + "\n" + string(c.Body()) + string(c.BodyRaw())
+		fmt.Printf("%v", msg)
+		return c.SendString("StatusBadRequest")
+	}
+
+	// Get the expected password from our in memory map
+	expectedPassword, ok := users[creds.Username]
+
+	// If a password exists for the given user
+	// AND, if it is the same as the password we received, the we can move ahead
+	// if NOT, then we return an "Unauthorized" status
+	if !ok || expectedPassword != creds.Password {
+		return c.SendString("StatusUnauthorized")
+	}
+
+	// Declare the expiration time of the token
+	// here, we have kept it as 5 minutes
+	expirationTime := time.Now().Add(5 * time.Minute)
+	// Create the JWT claims, which includes the username and expiry time
+	claims := &Claims{
+		Username: creds.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			// In JWT, the expiry time is expressed as unix milliseconds
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+		},
+	}
+
+	// Declare the token with the algorithm used for signing, and the claims
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Create the JWT string
+	tokenString, err := token.SignedString(jwtKey)
+	if err != nil {
+		// If there is an error in creating the JWT return an internal server error
+		return c.SendString("StatusInternalServerError")
+	}
+
+	// Finally, we set the client cookie for "token" as the JWT we just generated
+	// we also set an expiry time which is the same as the token itself
+	c.Cookie(&fiber.Cookie{
+		Name:    "token",
+		Value:   tokenString,
+		Expires: expirationTime,
+	})
+	return c.SendString("done")
 }
